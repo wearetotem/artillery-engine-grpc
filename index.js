ce2d2fd05a2a18f7afef2adfebc3a59dd893b593
Kyle Osborn <kyle.osborn@wearetotem.io>
2024-09-05 13:41:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const A = require('async')
-const grpc = require('grpc')
+const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
 const debug = require('debug')('engine:grpc')
 
