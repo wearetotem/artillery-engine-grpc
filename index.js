bd80e736f826a1ea8315d1cbab40ef259e9b4d36
Kyle Osborn <kyle.osborn@wearetotem.io>
2024-09-05 13:47:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const A = require('async')
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
+const connect = require('@bufbuild/connect-node')
 const debug = require('debug')('engine:grpc')
 
 const { log: logger } = console
@@ -13,7 +12,6 @@ function ArtilleryGRPCEngine(script, ee, helpers) {
   const { config } = this.script
   debug('script.config=%O', config)
 
-  // Hash<K, V>: K=target, V=grPC client
   this.serviceClient = this.loadServiceClient()
   const client = this.initGRPCClient(config.target)
   this.clientHash = {
@@ -26,31 +24,22 @@ function ArtilleryGRPCEngine(script, ee, helpers) {
 ArtilleryGRPCEngine.prototype.loadServiceClient = function () {
   const {
     protobufDefinition,
-    protoLoaderConfig,
   } = this.getEngineConfig()
 
   debug('protobufDefinition=%O', protobufDefinition)
-  debug('protoLoaderConfig=%O', protoLoaderConfig)
 
   const {
     filepath,
     service,
-    package,
   } = protobufDefinition
 
-  // @return GrpcObject
-  function loadPackageDefinition() {
-    const packageDefinition = protoLoader.loadSync(
-      filepath,
-      protoLoaderConfig || {},
-    )
-    return grpc.loadPackageDefinition(packageDefinition)
-  }
+  const { ProtoLoader } = require('@bufbuild/protobuf')
 
-  const grpcObject = loadPackageDefinition()
-  const packages = package.split('.')
-  const services = packages.reduce((obj, p) => obj = obj[p], grpcObject)
-  return services[service]
+  // Load the service definition using ProtoLoader
+  const root = ProtoLoader.loadSync(filepath)
+  const ServiceClient = root.lookupService(service)
+
+  return ServiceClient
 }
 
 /**
@@ -60,33 +49,27 @@ ArtilleryGRPCEngine.prototype.getEngineConfig = function () {
   const {
     channelOpts,
     protobufDefinition,
-    protoLoaderConfig,
     metadata,
   } = this.script.config.engines.grpc
 
   return {
     channelOpts,
     protobufDefinition,
-    protoLoaderConfig,
     metadata,
   }
 }
 
 ArtilleryGRPCEngine.prototype.initGRPCClient = function (target) {
   const { channelOpts } = this.getEngineConfig()
-  /**
-   * Filter out invalid channelOpts for gRPC client.
-   * Channel third argument must be "an object with string keys and integer or string values"
-   *
-   * @see https://github.com/kenju/artillery-engine-grpc/pull/8/files#issuecomment-594329331
-   */
-  const opts = Object.keys(channelOpts).reduce((acc, k) => {
-    if (typeof channelOpts[k] === "string" || typeof channelOpts[k] === "number") {
-      acc[k] = channelOpts[k]
-    }
-    return acc
-  }, {})
-  return new this.serviceClient(target, grpc.credentials.createInsecure(), opts)
+
+  // Connect RPC does not use channel options in the same way, but options can be passed
+  const options = {
+    http2: channelOpts.http2 || false, // Example: adjust according to your config
+  }
+
+  // Initialize the Connect RPC client
+  const client = new connect.Client(this.serviceClient, target, options)
+  return client
 }
 
 ArtilleryGRPCEngine.prototype.createScenario = function (scenarioSpec, ee) {
@@ -95,16 +78,15 @@ ArtilleryGRPCEngine.prototype.createScenario = function (scenarioSpec, ee) {
   return this.compile(tasks, scenarioSpec.flow, ee)
 }
 
-/**
- * @doc https://grpc.github.io/grpc/node/grpc.Metadata.html
- **/
 ArtilleryGRPCEngine.prototype.buildGRPCMetadata = function () {
   const { metadata } = this.getEngineConfig()
-  const grpcMetadata = new grpc.Metadata();
+  const headers = {}
+
   Object.entries(metadata).forEach(([k, v]) => {
-    grpcMetadata.add(k, v)
+    headers[k] = v
   })
-  return grpcMetadata
+
+  return headers
 }
 
 ArtilleryGRPCEngine.prototype.step = function step(ops, ee, scenarioSpec) {
@@ -124,52 +106,49 @@ ArtilleryGRPCEngine.prototype.step = function step(ops, ee, scenarioSpec) {
       ee.emit('counter', 'engine.grpc.codes.' + error.code, 1);
     } else {
       ee.emit('counter', 'engine.grpc.responses.success', 1)
-      ee.emit('counter', 'engine.grpc.codes.' + grpc.status.OK, 1);
+      ee.emit('counter', 'engine.grpc.codes.' + connect.Code.OK, 1);
     }
 
-    /** @doc https://nodejs.org/api/process.html#process_process_hrtime_time */
     const [diffSec, diffNanosec] = process.hrtime(startedAt)
-    const deltaNanosec = (diffSec * 1e9) + diffNanosec // NOTE: 1e9 means 1 * 10 to the 9th power, which is 1 billion (1000000000).
-    const deltaMillisec = deltaNanosec / 1e6 // NOTE: 1e6 means 1 * 10 to the 6th power, which is 1 million (1000000).
+    const deltaNanosec = (diffSec * 1e9) + diffNanosec
+    const deltaMillisec = deltaNanosec / 1e6
     ee.emit('histogram', 'engine.grpc.response_time', deltaMillisec)
   }
 
   function beforeRequestHook(context, config, scenarioSpec) {
     if (!scenarioSpec.beforeRequest) { return; }
 
-    // call beforeRequest hooks
     Array.from(scenarioSpec.beforeRequest).forEach((functionName) => {
       const f = config.processor[functionName]
       if (f) { f(null, context, null, () => {}) }
     })
   }
 
-  // gRPC request
-  return gRPCRequest = (context, callback) => {
+  return (context, callback) => {
     beforeRequestHook(context, this.script.config, scenarioSpec)
 
     const target = context.vars.target
     let client = this.clientHash[target]
     if (!client) {
       client = this.initGRPCClient(target)
-      this.clientHash[target] = client // memoize
+      this.clientHash[target] = client
     }
-    const grpcMetadata = this.buildGRPCMetadata()
+    const headers = this.buildGRPCMetadata()
 
     Object.keys(ops).map((rpcName) => {
       const args = this.helpers.template(ops[rpcName], context)
-      /** @doc https://grpc.github.io/grpc/node/grpc.Client.html */
-      client[rpcName](args, grpcMetadata, (error, response) => {
 
-        recordMetrics(startedAt, error, response)
-
-        if (error) {
+      // Make a Connect RPC request
+      client.rpc(rpcName, args, { headers })
+        .then(response => {
+          recordMetrics(startedAt, null, response)
+          callback(null, context)
+        })
+        .catch(error => {
+          recordMetrics(startedAt, error, null)
           ee.emit('error', error)
-          return callback(error, context)
-        } else {
-          return callback(null, context)
-        }
-      })
+          callback(error, context)
+        })
     })
   }
 }
